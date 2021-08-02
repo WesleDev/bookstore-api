3323ce0c8991087720564b1cc3fac7dae772c6bc
WesleDev <wesle.profissional@gmail.com>
2021-08-02 20:51:25 -0300

    finalizando classes de dominio

--- src/main/java/com/wesle/bookstore/domain/Livro.java
+++ src/main/java/com/wesle/bookstore/domain/Livro.java
@@ -1,13 +1,27 @@
 package com.wesle.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+public class Livro implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String title;
 	private String name_author;
 	private String text;
 
+	@ManyToOne
+	@JoinColumn(name = "category_id")
 	private Categoria category;
 
 	public Livro() {
