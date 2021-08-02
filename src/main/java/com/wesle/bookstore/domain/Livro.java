1428a6b9d6c705602362cd0b6271fc15202b44cd
WesleDev <wesle.profissional@gmail.com>
2021-08-02 20:32:57 -0300

    classes de dominio iniciadas

---
+++ src/main/java/com/wesle/bookstore/domain/Livro.java
@@ -0,0 +1,84 @@
+package com.wesle.bookstore.domain;
+
+import java.util.Objects;
+
+public class Livro {
+	private Integer id;
+	private String title;
+	private String name_author;
+	private String text;
+
+	private Categoria category;
+
+	public Livro() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Livro(Integer id, String title, String name_author, String text, Categoria category) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.name_author = name_author;
+		this.text = text;
+		this.category = category;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getName_author() {
+		return name_author;
+	}
+
+	public void setName_author(String name_author) {
+		this.name_author = name_author;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Categoria getCategory() {
+		return category;
+	}
+
+	public void setCategory(Categoria category) {
+		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
