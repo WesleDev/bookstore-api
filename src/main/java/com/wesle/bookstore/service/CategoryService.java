a94c14303b9dac611f52f4ecf4e5213ed2de6255
WesleDev <wesle.profissional@gmail.com>
2021-08-14 21:20:09 -0300

    UPDATE para categoria

--- src/main/java/com/wesle/bookstore/service/CategoryService.java
+++ src/main/java/com/wesle/bookstore/service/CategoryService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wesle.bookstore.domain.Category;
+import com.wesle.bookstore.dto.CategoryDTO;
 import com.wesle.bookstore.repositories.CategoryRepository;
 import com.wesle.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -28,6 +29,13 @@ public class CategoryService {
 	
 	public Category create(Category obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Category update(Integer id, CategoryDTO objDto) {
+		Category obj = findById(id);
+		obj.setName(objDto.getName());
+		obj.setDescription(objDto.getDescription());
 		return repository.save(obj);
 	}
 }
